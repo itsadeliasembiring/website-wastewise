68c43947c7f4785d3684e6e3eb68380e8e308989
itsadeliasembiring <adeliasmilecontact@gmail.com>
2025-03-17 19:48:23 +0700

    Updated

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,11 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('auth/login');
 })->name('login');
-
 Route::get('/register', function () {
+    return view('auth/register');
+})->name('register');
+
+
+Route::get('/register/ayam', function () {
     return view('auth/register');
 })->name('register');
