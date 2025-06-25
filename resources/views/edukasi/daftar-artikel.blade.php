394a4bd1f24a5e1789d3cfb37031102a016316d1
itsadeliasembiring <adeliasmilecontact@gmail.com>
2025-06-25 16:13:11 +0700

    updated

--- resources/views/edukasi/daftar-artikel.blade.php
+++ resources/views/edukasi/daftar-artikel.blade.php
@@ -38,9 +38,9 @@
                                 placeholder="Cari artikel..." 
                                 class="w-full pl-10 pr-4 py-3 border text-black border-gray-300 rounded-lg focus:ring-2 focus:ring-[#3D8D7A] focus:border-transparent"
                             >
-                            <svg class="absolute left-3 top-[70%] transform -translate-y-1/2 h-5 w-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <!-- <svg class="absolute left-3 top-[70%] transform -translate-y-1/2 h-5 w-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                            </svg>
+                            </svg> -->
                         </div>
                         <button 
                             type="submit"
